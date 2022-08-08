0b09d20b5eae27d4098a3ecca59e46ceb961c90b
Arindam <102957273+babudan@users.noreply.github.com>
2022-08-08 21:37:29 +0530

    change in route

--- src/routes/route.js
+++ src/routes/route.js
@@ -7,23 +7,23 @@ router.get('/GET/movies', function (req, res) {
          res.send(movies);
 });
 
-router.get('/GET/movies/:indexNumber', function (req, res){
-      const movies = ["Tenet","The Btamna Begins","Pather Pachali","Charlie The Choclate Factory"];
-      const index = req.params.indexNumber;
-      console.log(movies[index]);
-      res.send(movies[index]);
-})
-router.get('/GET/movies/:indexNumber', function (req, res){
-    const movies = ["Tenet","The Btamna Begins","Pather Pachali","Charlie The Choclate Factory"];
-    const index = req.params.indexNumber;
-       if(index<3){
-        res.send(movies[index]);
-        console.log(movies[index]);
-       }else{
-        res.send("use a valid index");
-        console.log("use a valid index");
-       }
-});
+// router.get('/GET/movies/:indexNumber', function (req, res){
+//       const movies = ["Tenet","The Btamna Begins","Pather Pachali","Charlie The Choclate Factory"];
+//       const index = req.params.indexNumber;
+//       console.log(movies[index]);
+//       res.send(movies[index]);
+// })
+// router.get('/GET/movies/:indexNumber', function (req, res){
+//     const movies = ["Tenet","The Btamna Begins","Pather Pachali","Charlie The Choclate Factory"];
+//     const index = req.params.indexNumber;
+//        if(index<4){
+//         res.send(movies[index]);
+//         console.log(movies[index]);
+//        }else{
+//         res.send("use a valid index");
+//         console.log("use a valid index");
+//        }
+// });
 
 router.get('/GET/films', function (req, res){
        const films = [{
@@ -56,7 +56,7 @@ router.get('/GET/films/:filmid', function (req, res){
     "name": "Finding Nemo"
     }];
     const index1 = req.params.filmid;
-    if(index1<5 && index1>0){
+    if(index1<5 && index1>0 ){
     res.send(films[index1-1]);
     console.log(films[index1-1]);
     }else{
