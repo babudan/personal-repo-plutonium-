b5da588a517f74844da56e9e474c559256e0a283
Arindam <102957273+babudan@users.noreply.github.com>
2022-08-13 19:10:25 +0530

    little change

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,15 +1,35 @@
 const UserModel= require("../models/userModel")
 
-const createUser= async function (req, res) {
-    let data= req.body
-    let savedData= await UserModel.create(data)
-    res.send({msg: savedData})
-}
+// const createUser= async function (req, res) {
+//     let data= req.body
+//     let savedData= await UserModel.create(data)
+//     res.send({msg: savedData})
+// }
 
-const getUsersData= async function (req, res) {
-    let allUsers= await UserModel.find()
-    res.send({msg: allUsers})
-}
+// const getUsersData= async function (req, res) {
+//     let allUsers= await UserModel.find()
+//     res.send({msg: allUsers})
+// }
 
-module.exports.createUser= createUser
-module.exports.getUsersData= getUsersData
+const createNewBook= async function (req, res) {
+       let data =  req.body;
+        let newbooks = await UserModel.create(data);
+        res.send({msg: newbooks});
+    }
+
+    const getAllBook = async function (req, res) {
+         let allbooks= await UserModel.find();
+         res.send({msg: allbooks});
+     }
+
+     module.exports.getAllBook = getAllBook;
+    module.exports.createNewBook = createNewBook;
+    
+
+
+
+
+
+
+// module.exports.createUser= createUser
+// module.exports.getUsersData= getUsersData
--- src/routes/route.js
+++ src/routes/route.js
@@ -3,12 +3,19 @@ const router = express.Router();
 const UserModel= require("../models/userModel.js")
 const UserController= require("../controllers/userController")
 
-router.get("/test-me", function (req, res) {
-    res.send("My first ever api!")
-})
+// router.get("/test-me", function (req, res) {
+//     res.send("My first ever api!")
+// })
 
-router.post("/createUser", UserController.createUser  )
+// router.post("/createUser", UserController.createUser  )
 
-router.get("/getUsersData", UserController.getUsersData)
+// router.get("/getUsersData", UserController.getUsersData)
+
+
+
+
+router.post("/createBook", UserController.createNewBook);
+router.get("/getBook", UserController.getAllBook);
+
 
 module.exports = router;
