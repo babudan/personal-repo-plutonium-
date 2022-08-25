ebc86457c894f6e55c88651749dc1a5e5bc9e939
Arindam <102957273+babudan@users.noreply.github.com>
2022-08-26 00:02:56 +0530

    change

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -23,8 +23,19 @@ const getUsersData= async function (req, res) {
     res.send({msg: allUsers})
 }
 
+
+// ASSIGNMENT:-
+// Write a middleware that logs (console.log) some data everytime any API is hit
+// Data to be logged:-the current timestamp(as date time) , the IP of the user and the route being requested).
+// For this first figure out how to get the route location being request, how to get current timestamp and how to get the IP.
+// NOTE: ip of local computer will come as ::1 so dont get disturbed by seeing this)
+//  
+// e.g: you should be logging something like this on each line:
+// time , IP, Route should be printed on each line in terminal( every time an api is hit)
+// 2010-08-19 14:00:00 , 123.459.898.734 , /createUser
+
 const code = async function(req,res,next){
-    let data  = req.param;
+    let data  = req.body;
     console.log(data);
     next();
 }
--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ const bodyParser = require('body-parser');
 const route = require('./routes/route.js');
 const { default: mongoose } = require('mongoose');
 const app = express();
-const commonMW = require ("./middlewares/commonMiddlewares")
+// const commonMW = require ("./middlewares/commonMiddlewares")
 const moment = require ("moment");
 var ip = require("ip");
 app.use(bodyParser.json());
@@ -16,12 +16,6 @@ mongoose.connect("mongodb+srv://ArindamDan:Arindam12345@cluster0.nl5g5.mongodb.n
 .then( () => console.log("MongoDb is connected"))
 .catch ( err => console.log(err) )
 
-// app.use (
-//     function (req, res, next) {
-//         console.log ("inside GLOBAL MW");
-//         next();
-//   }
-//   );
 
 
 
@@ -32,18 +26,18 @@ app.use(
         const date = moment().format("DD-MM-YYYY , hh:mm:ss");
         console.log(date);
         //     // var adress = ipadress.getClientIp(req);
-             var a = (req.headers['x-forwarded-for'] || '').split(',').pop().trim() || req.socket.remoteAddress
+            //  var a = (req.headers['x-forwarded-for'] || '').split(',').pop().trim() || req.socket.remoteAddress
         //     // var ip = req.ip.split(':ffff:x.x.x.x').pop();
         //     // var ipInfo = getIP(req);
         //     //  console.log(ipInfo);
-            // console.log(ip.address());
-            var b = req.ip;
-            console.log(b.toString());
+            console.log(ip.address());
+            // var b = req.ip;
+            // console.log(b.toString());
                 var rou = req.path;
                 console.log(rou);
                 res.send({msg : "end"});
     }
-)
+);
 
 
 
