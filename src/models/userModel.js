26bf347aa4389681bcd65fa8d36358661217be50
Arindam <102957273+babudan@users.noreply.github.com>
2022-08-28 23:56:59 +0530

    change

--- src/models/userModel.js
+++ src/models/userModel.js
@@ -1,20 +1,17 @@
 const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema( {
-    firstName: String,
-    lastName: String,
-    mobile: {
-        type: String,
-
-        required: true
+    "firstName" : String,
+    "lastName" : String,
+    "mobile" : String,
+    "emailId" : String,
+    "password" : String,
+    "gender" : String,
+	"isDeleted": {
+        type :Boolean,
+        default :false
     },
-    emailId: String,
-    password: String,
-    gender: {
-        type: String,
-        enum: ["male", "female", "other"]
-    },
-    age: Number,
+    "age" : Number
 }, { timestamps: true });
 
-module.exports = mongoose.model('User', userSchema)
+module.exports = mongoose.model('Jwtuser', userSchema)
